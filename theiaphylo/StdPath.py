d0736f77e8ba73e9f15a06fe316d8a58e4fe615f
xonq <konkelzach@protonmail.com>
2025-03-18 18:55:24 +0000

    init path formatting module

---
+++ theiaphylo/StdPath.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import os
+import re
+
+class Path(str):
+    """A standardized class for handling file paths"""
+    def __init__(self, path):
+        self.path = self.format_path(path)
+
+    def expand_env_var(self):
+        """Expands environment variables by regex substitution"""
+        # identify all environment variables in the self.path
+        envs = re.findall(r'\$[^/]+', self.path)
+        # replace the environment variables with their values
+        for env in envs:
+            self.path = self.path.replace(env, os.environ[env.replace('$','')])
+        return self.path.replace('//','/')
+    
+    def format_path(self):
+        """Convert all self.path types to absolute self.path with explicit directory ending"""
+        if self.path:
+            # expand username
+            self.path = os.path.expanduser(self.path)
+            # expand environment variables
+            self.path = expand_env_var(self.path)
+            # only save the directory ending if it is a directory
+            if self.path.endswith('/'):
+                if not os.self.path.isdir(self.path):
+                    self.path = self.path[:-1]
+            # add the directory ending if it is a directory
+            else:
+                if os.path.isdir(self.path):
+                    self.path += '/'
+            # make the self.path absolute
+            if not self.path.startswith('/'):
+                self.path = os.getcwd() + '/' + self.path
+            # replace redundancies
+            self.path = self.path.replace('/./', '/')
+            # trace back to the root directory
+            while '/../' in self.path:
+                self.path = re.sub(r'[^/]+/\.\./(.*)', r'\1', self.path)
+        return self.path
+    
+ 
