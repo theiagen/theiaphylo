d5b1ae7093d8db7a8ba12e1867aae0ebb7118506
xonq <konkelzach@protonmail.com>
2025-03-19 13:22:29 +0000

    enhance argument parsing for tree comparison; add distance metrics options and set defaults

--- theiaphylo/phylocompare.py
+++ theiaphylo/phylocompare.py
@@ -54,18 +54,29 @@ def main(args, output_file = 'phylocompare_results.txt'):
     tree2 = import_tree(Path(args.tree2), outgroup = outgroup, midpoint = args.midpoint)
 
     # compare the trees
-    tree_res = compare_trees(tree1, tree2, rooted = rooted)
+    tree_res = compare_trees(tree1, tree2, rooted = rooted, 
+                             mc = args.matching_cluster, rf = args.robinson_foulds, lrm = args.lin_rajan_moret)
 
     # output the results
     output_results(output_file, tree_res)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Compare phylogenetic trees')
-    parser.add_argument('-t1', '--tree1', required = True, help = 'First tree file')
-    parser.add_argument('-t2', '--tree2', required = True, help = 'Second tree file')
-    parser.add_argument('-o', '--outgroup', help = 'Comma-delimited list of outgroup tips for rooting trees')
-    parser.add_argument('-m', '--midpoint', action = 'store_true', help = 'Root trees at midpoint')
-    parser.add_argument('-d', '--debug', action = 'store_true', help = 'Enable debug mode')
+    parser = argparse.ArgumentParser(description = 'Compare phylogenetic trees by distance metrics: ' \
+                                     + 'Robinson-Foulds, Matching Cluster, and Lin-Rajan-Moret')
+    in_args = parser.add_argument_group('Inputs')
+    in_args.add_argument('tree1', help = 'First tree file')
+    in_args.add_argument('tree2', help = 'Second tree file')
+
+    phy_args = parser.add_argument_group('Phylogenetics Options')
+    phy_args.add_argument('-o', '--outgroup', help = 'Comma-delimited list of outgroup tips for rooting trees')
+    phy_args.add_argument('-m', '--midpoint', action = 'store_true', help = 'Root trees at midpoint')
+    phy_args.add_argument('-mc', '--matching_cluster', action = 'store_true', help = 'Calculate matching cluster distance; overwrites default: ALL')
+    phy_args.add_argument('-rf', '--robinson_foulds', action = 'store_true', help = 'Calculate Robinson-Foulds distance; overwrites default: ALL')
+    phy_args.add_argument('-lrm', '--lin_rajan_moret', action = 'store_true', help = 'Calculate Lin-Rajan-Moret distance; overwrites default: ALL')
+
+    run_args = parser.add_argument_group('Run Options')
+    run_args.add_argument('-d', '--debug', action = 'store_true', help = 'Enable debug mode')
+
     args = parser.parse_args()
 
     if args.debug:
@@ -75,6 +86,12 @@ if __name__ == '__main__':
         logging.basicConfig(level = logging.INFO,
                             format = '%(asctime)s - %(levelname)s - %(message)s') 
     logger = logging.getLogger(__name__)
+
+    # run all comparisons by default
+    if not args.matching_cluster and not args.robinson_foulds and not args.lin_rajan_moret:
+        args.matching_cluster = True
+        args.robinson_foulds = True
+        args.lin_rajan_moret = True
 
     main(args)
     sys.exit(0)
