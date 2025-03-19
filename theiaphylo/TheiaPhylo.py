2a3a4841b278c31b1bccfd13cd9ee4819562791d
xonq <konkelzach@protonmail.com>
2025-03-19 13:27:04 +0000

    pep8 formatting

--- theiaphylo/TheiaPhylo.py
+++ theiaphylo/TheiaPhylo.py
@@ -8,44 +8,52 @@ from cogent3 import load_tree
 
 logger = logging.getLogger(__name__)
 
+
 class RootError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
-def root_tree(tree, outgroup = [], midpoint = False):
+
+def root_tree(tree, outgroup=[], midpoint=False):
     """Root the tree based on outgroup(s)"""
     if midpoint:
         # root at the midpoint
         return tree.root_at_midpoint()
-    elif isinstance(outgroup, str) or (isinstance(outgroup, list) and len(outgroup) == 1):
+    elif isinstance(outgroup, str) or (
+        isinstance(outgroup, list) and len(outgroup) == 1
+    ):
         # root via outgroup tip if there is 1 list member or it is a string
         if isinstance(outgroup, list):
             outgroup = outgroup[0]
         return tree.rooted_with_tip(outgroup).bifurcating()
     elif outgroup:
-        # root by determining the MRCA branch as the MRCA that contains as few tips as possible 
+        # root by determining the MRCA branch as the MRCA that contains as few tips as possible
         # while including those delineated in the outgroup list
-        nodes = {k: (v, len(v.get_tip_names())) \
-                 for k, v in tree.get_nodes_dict().items() \
-                    if set(outgroup).issubset(set(v.get_tip_names()))}
+        nodes = {
+            k: (v, len(v.get_tip_names()))
+            for k, v in tree.get_nodes_dict().items()
+            if set(outgroup).issubset(set(v.get_tip_names()))
+        }
         mrca_tip_len = min([v[1] for v in list(nodes.values())])
         mrca_edge = [k for k, v in nodes.items() if v[1] == mrca_tip_len][0]
         try:
             return tree.rooted_at(mrca_edge).bifurcating()
         except:
-            raise RootError(f'tree could not be rooted with supplied tips: ' \
-                    + f'{outgroup}')
+            raise RootError(
+                f"tree could not be rooted with supplied tips: " + f"{outgroup}"
+            )
     else:
-        raise RootError(f'no outgroup provided')
+        raise RootError(f"no outgroup provided")
 
-def import_tree(tree_path, outgroup = [], midpoint = False):
+
+def import_tree(tree_path, outgroup=[], midpoint=False):
     """Import a phylogenetic tree"""
     tree = load_tree(tree_path)
     if outgroup or midpoint:
         rooted_tree = root_tree(tree, outgroup, midpoint)
-        logger.debug(f'Rooted {tree_path}:\n{rooted_tree.ascii_art()}')
+        logger.debug(f"Rooted {tree_path}:\n{rooted_tree.ascii_art()}")
         return rooted_tree
     else:
-        logger.debug(f'{tree_path}:\n{tree.ascii_art()}') 
+        logger.debug(f"{tree_path}:\n{tree.ascii_art()}")
         return tree
--- theiaphylo/phylocompare.py
+++ theiaphylo/phylocompare.py
@@ -6,89 +6,134 @@ import argparse
 from TheiaPhylo import *
 from StdPath import Path
 
-def compare_trees(tree1, tree2, mc = True, rf = True, lrm = True, rooted = True):
+
+def compare_trees(tree1, tree2, mc=True, rf=True, lrm=True, rooted=True):
     """Quantify tree distances between two phylogenetic trees"""
     mc_dist, rf_dist, lrm_dist = None, None, None
     if rooted:
         # compare rooted trees
         if rf:
             # calculate the Robinson-Foulds distance
-            rf_dist = tree1.tree_distance(tree2, method='rooted_robinson_foulds')
+            rf_dist = tree1.tree_distance(tree2, method="rooted_robinson_foulds")
         if mc:
             # calculate the matching cluster distance
-            mc_dist = tree1.tree_distance(tree2, method='matching_cluster')
+            mc_dist = tree1.tree_distance(tree2, method="matching_cluster")
     else:
         # compare unrooted trees
         if lrm:
             # calculate the least lin rajan moret distance
-            lrm_dist = tree1.tree_distance(tree2, method='lin_rajan_moret')
+            lrm_dist = tree1.tree_distance(tree2, method="lin_rajan_moret")
         if rf:
             # calculate the Robinson-Foulds distance
-            rf_dist = tree1.tree_distance(tree2, method='unrooted_robinson_foulds')
+            rf_dist = tree1.tree_distance(tree2, method="unrooted_robinson_foulds")
     return mc_dist, rf_dist, lrm_dist
+
 
 def output_results(res_path, tree_res):
     """Output the results of the tree comparison"""
-    with open(res_path, 'w') as res_file:
-        res_file.write('#robinson_foulds_distance\tmatching_cluster_distance\tlin-rajan-moret_distance\n')
-        res_file.write(f'{tree_res[0]}\t{tree_res[1]}\t{tree_res[2]}\n')
+    with open(res_path, "w") as res_file:
+        res_file.write(
+            "#robinson_foulds_distance\tmatching_cluster_distance\tlin-rajan-moret_distance\n"
+        )
+        res_file.write(f"{tree_res[0]}\t{tree_res[1]}\t{tree_res[2]}\n")
 
-def main(args, output_file = 'phylocompare_results.txt'):
+
+def main(args, output_file="phylocompare_results.txt"):
     """Main function"""
     outgroup = []
     if args.outgroup and args.midpoint:
-        raise ValueError('cannot root trees at midpoint and with outgroup simultaneously')
+        raise ValueError(
+            "cannot root trees at midpoint and with outgroup simultaneously"
+        )
     elif args.outgroup or args.midpoint:
         rooted = True
         if args.outgroup:
-            outgroup = args.outgroup.split(',')
+            outgroup = args.outgroup.split(",")
             # CURRENTLY NOT FUNCTIONAL WITH MULTIPLE OUTGROUPS
             if len(outgroup) > 1:
-                raise RootError('multiple outgroups not supported')
+                raise RootError("multiple outgroups not supported")
     else:
         # CURRENTLY NOT FUNCTIONAL WITHOUT EXPLICIT ROOT
-        raise ValueError('no rooting method provided')
-    
+        raise ValueError("no rooting method provided")
+
     # import the trees
-    tree1 = import_tree(Path(args.tree1), outgroup = outgroup, midpoint = args.midpoint)
-    tree2 = import_tree(Path(args.tree2), outgroup = outgroup, midpoint = args.midpoint)
+    tree1 = import_tree(Path(args.tree1), outgroup=outgroup, midpoint=args.midpoint)
+    tree2 = import_tree(Path(args.tree2), outgroup=outgroup, midpoint=args.midpoint)
 
     # compare the trees
-    tree_res = compare_trees(tree1, tree2, rooted = rooted, 
-                             mc = args.matching_cluster, rf = args.robinson_foulds, lrm = args.lin_rajan_moret)
+    tree_res = compare_trees(
+        tree1,
+        tree2,
+        rooted=rooted,
+        mc=args.matching_cluster,
+        rf=args.robinson_foulds,
+        lrm=args.lin_rajan_moret,
+    )
 
     # output the results
     output_results(output_file, tree_res)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Compare phylogenetic trees by distance metrics: ' \
-                                     + 'Robinson-Foulds, Matching Cluster, and Lin-Rajan-Moret')
-    in_args = parser.add_argument_group('Inputs')
-    in_args.add_argument('tree1', help = 'First tree file')
-    in_args.add_argument('tree2', help = 'Second tree file')
 
-    phy_args = parser.add_argument_group('Phylogenetics Options')
-    phy_args.add_argument('-o', '--outgroup', help = 'Comma-delimited list of outgroup tips for rooting trees')
-    phy_args.add_argument('-m', '--midpoint', action = 'store_true', help = 'Root trees at midpoint')
-    phy_args.add_argument('-mc', '--matching_cluster', action = 'store_true', help = 'Calculate matching cluster distance; overwrites default: ALL')
-    phy_args.add_argument('-rf', '--robinson_foulds', action = 'store_true', help = 'Calculate Robinson-Foulds distance; overwrites default: ALL')
-    phy_args.add_argument('-lrm', '--lin_rajan_moret', action = 'store_true', help = 'Calculate Lin-Rajan-Moret distance; overwrites default: ALL')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Compare phylogenetic trees by distance metrics: "
+        + "Robinson-Foulds, Matching Cluster, and Lin-Rajan-Moret"
+    )
+    in_args = parser.add_argument_group("Inputs")
+    in_args.add_argument("tree1", help="First tree file")
+    in_args.add_argument("tree2", help="Second tree file")
 
-    run_args = parser.add_argument_group('Run Options')
-    run_args.add_argument('-d', '--debug', action = 'store_true', help = 'Enable debug mode')
+    phy_args = parser.add_argument_group("Phylogenetics Options")
+    phy_args.add_argument(
+        "-o",
+        "--outgroup",
+        help="Comma-delimited list of outgroup tips for rooting trees",
+    )
+    phy_args.add_argument(
+        "-m", "--midpoint", action="store_true", help="Root trees at midpoint"
+    )
+    phy_args.add_argument(
+        "-mc",
+        "--matching_cluster",
+        action="store_true",
+        help="Calculate matching cluster distance; overwrites default: ALL",
+    )
+    phy_args.add_argument(
+        "-rf",
+        "--robinson_foulds",
+        action="store_true",
+        help="Calculate Robinson-Foulds distance; overwrites default: ALL",
+    )
+    phy_args.add_argument(
+        "-lrm",
+        "--lin_rajan_moret",
+        action="store_true",
+        help="Calculate Lin-Rajan-Moret distance; overwrites default: ALL",
+    )
+
+    run_args = parser.add_argument_group("Run Options")
+    run_args.add_argument(
+        "-d", "--debug", action="store_true", help="Enable debug mode"
+    )
 
     args = parser.parse_args()
 
     if args.debug:
-        logging.basicConfig(level = logging.DEBUG,
-                            format = '%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(
+            level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
     else:
-        logging.basicConfig(level = logging.INFO,
-                            format = '%(asctime)s - %(levelname)s - %(message)s') 
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
     logger = logging.getLogger(__name__)
 
     # run all comparisons by default
-    if not args.matching_cluster and not args.robinson_foulds and not args.lin_rajan_moret:
+    if (
+        not args.matching_cluster
+        and not args.robinson_foulds
+        and not args.lin_rajan_moret
+    ):
         args.matching_cluster = True
         args.robinson_foulds = True
         args.lin_rajan_moret = True
