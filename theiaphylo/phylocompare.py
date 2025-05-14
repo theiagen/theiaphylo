fa10206f41ec1bba1180af4120bb2495c1bd85e6
xonq <konkelzach@tuta.io>
2025-05-14 19:10:09 +0000

    relocate for packaging

--- theiaphylo/phylocompare.py
+++ theiaphylo/phylocompare.py
@@ -3,8 +3,8 @@
 import sys
 import logging
 import argparse
-from TheiaPhylo import *
-from StdPath import Path
+from lib.TheiaPhylo import *
+from lib.StdPath import Path
 from __init__ import __VERSION__
 
 
